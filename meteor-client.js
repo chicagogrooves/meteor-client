7c53b4bf2317b4a64968dae49f9c9f62715af8bb
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-15 13:20:31 -0500

    Remove html libs

--- meteor-client.js
+++ meteor-client.js
@@ -7,12 +7,7 @@ module.exports = function(jQuery,underscore) {
   require("meteor-ordered-dict")(Meteor);
   require("meteor-tracker")(Meteor);
   require("meteor-observe-sequence")(Meteor);
-  require("meteor-htmljs")(Meteor);
-  require("meteor-html-tools")(Meteor);
   require("meteor-reactive-var")(Meteor);
   require("meteor-reactive-object-map")(Meteor);
-  require("meteor-templating")(Meteor);
-  require("meteor-spacebars")(Meteor);
-  require("meteor-spacebars-compiler")(Meteor);
   return Meteor;
 };
